f4faa690e1ad5c50332b0d0939f332339acbb30e
Xanible <cbp1222@jagmail.southalabama.edu>
2017-02-08 11:26:29 -0600

    I dont even know

--- src/opcodecounter/Main.java
+++ src/opcodecounter/Main.java
@@ -22,6 +22,7 @@ public class Main {
 		File benDir = new File("C:\\Users\\ColbyAdmin\\Desktop\\Test\\Benign");
 		File[] malFiles = malDir.listFiles();
 		File[] benFiles = benDir.listFiles();
+		int malAmount = 0, benAmount = 0;
 		
 		
 
@@ -40,6 +41,7 @@ public class Main {
 					} else {
 						malOpcodes.put(line, 1);
 					}
+					malAmount++;
 				}
 				br.close();
 			} catch (FileNotFoundException e) {
@@ -64,6 +66,7 @@ public class Main {
 					} else {
 						benOpcodes.put(line, 1);
 					} 
+					benAmount++;
 				}
 				br.close();
 			} catch (FileNotFoundException e) {
@@ -73,30 +76,108 @@ public class Main {
 			}
 		}
 		
-		// Output results
+		// Calculate and Output results
 		try {
 			BufferedWriter bw = new BufferedWriter(new FileWriter("C:\\Users\\ColbyAdmin\\Desktop\\Test\\results.txt"));
 			for(String s : opcodes) {
+				int opMal, opBen;
 				bw.write(s);
 				bw.write(" ");
 				if(malOpcodes.containsKey(s)){
-					bw.write(String.valueOf(malOpcodes.get(s)));
+					opMal = malOpcodes.get(s);
+					bw.write(String.valueOf(opMal));
 					bw.write(" ");
 				} else {
+					opMal = 0;
 					bw.write(String.valueOf(0));
 					bw.write(" ");
 				}
 				if(benOpcodes.containsKey(s)){
-					bw.write(String.valueOf(benOpcodes.get(s)));
-					bw.newLine();;
+					opBen = benOpcodes.get(s);
+					bw.write(String.valueOf(opBen));
+					bw.write(" ");
 				} else {
+					opBen = 0;
 					bw.write(String.valueOf(0));
-					bw.newLine();
+					bw.write(" ");
 				}
+				double value = theEquation3(opMal, opBen, malAmount, benAmount, malFiles.length, benFiles.length);
+				bw.write(Double.toString(value));
+				bw.newLine();
 			}
 			bw.close();
 		} catch (IOException e) {
 			System.out.println("Error writing to source file.");
 		}
+	}
+	
+	/*
+	 * First interpretation of the mutual information equation. This version uses the frequency that the opcode appears in malware and the frequency it
+	 * appears in benign files instead of just relying on the total. Marginal probability is not used.
+	 */
+	public static double theEquation1(int opMal, int opBen, int malAmount, int benAmount, int malFiles, int benFiles) {
+		double pOM, pOB, pO, pM, pB, value;
+		pOM = (double)opMal/malAmount;
+		pOB = (double)opBen/benAmount;
+		pO = (double)(opMal + opBen)/(malAmount + benAmount);
+		pM = (double)malFiles/(malFiles + benFiles);
+		pB = (double)benFiles/(malFiles + benFiles);
+
+		System.out.println(pOM + " " + pOB + " " + pO + " " + pM + " " + pB);
+
+		value = pOM * Math.log((pOM/(pM * pO)));
+		System.out.println(value);
+		value += (1 - pOM) * Math.log(((1 - pOM)/(pM * (1 - pO))));
+		System.out.println(value);
+		value += pOB * Math.log((pOB/(pB * pO)));
+		System.out.println(value);
+		value += (1 - pOB) * Math.log(((1 - pOB)/(pB * (1 - pO))));
+		System.out.println(value);
+		return value;	
+	}
+	
+	/*
+	 * This is the interpretation of the equation that relies on marginal probability and the frequency that a given opcode appears across all files and
+	 * not just by category. 
+	 */
+	public static double theEquation2(int opMal, int opBen, int malAmount, int benAmount, int malFiles, int benFiles) {
+		double opcodeProb, malProb, benProb, value;
+		opcodeProb = (double)(opMal + opBen)/(malAmount + benAmount);
+		malProb = (double)malFiles/(malFiles + benFiles);
+		benProb = (double)benFiles/(malFiles + benFiles);
+		
+		value = (opcodeProb * malProb) * Math.log((opcodeProb * malProb)/(marginalProb(malProb, opcodeProb, (1 - opcodeProb)) * marginalProb(opcodeProb, malProb, benProb)));
+		//System.out.println(value);
+		value += ((1 - opcodeProb) * malProb) * Math.log(((1 - opcodeProb) * malProb)/(marginalProb(malProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
+		//System.out.println(value);
+		value += (opcodeProb * benProb) * Math.log((opcodeProb * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb(opcodeProb, malProb, benProb)));
+		//System.out.println(value);
+		value += ((1 - opcodeProb) * benProb) * Math.log(((1 - opcodeProb) * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
+		//System.out.println(value);
+		return value;	
+	}
+	
+	public static double theEquation3(int opMal, int opBen, int malAmount, int benAmount, int malFiles, int benFiles) {
+		double opcodeProb, malProb, benProb, value;
+		opcodeProb = (double)(opMal + opBen)/(malAmount + benAmount);
+		malProb = (double)malFiles/(malFiles + benFiles);
+		benProb = (double)benFiles/(malFiles + benFiles);
+		
+		value = (opcodeProb * malProb) * Math.log(((opcodeProb * malProb)/(marginalProb(malProb, opcodeProb, (1 - opcodeProb)) * marginalProb(opcodeProb, malProb, benProb))));
+		//System.out.println(value);
+		value += (1 - opcodeProb * malProb) * Math.log((1 - opcodeProb * malProb)/(marginalProb(malProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
+		//System.out.println(value);
+		value += (opcodeProb * benProb) * Math.log(((opcodeProb * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb(opcodeProb, malProb, benProb))));
+		//System.out.println(value);
+		value += (1 - opcodeProb * benProb) * Math.log((1 - opcodeProb * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
+		//System.out.println(value);
+		return value;	
+	}
+	
+	public static double marginalProb(double x, double y1, double y2) {
+		double value;
+		value = x * y1;
+		value += (x * y2);
+		return value;
 	}
 }
