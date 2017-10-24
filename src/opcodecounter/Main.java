12f87f67c645fb8060455535cca6929b12c39f43
Xanible <cbp1222@jagmail.southalabama.edu>
2017-10-24 17:05:31 -0500

    FINAL

--- src/opcodecounter/Main.java
+++ src/opcodecounter/Main.java
@@ -24,8 +24,6 @@ public class Main {
 		File[] benFiles = benDir.listFiles();
 		int malAmount = 0, benAmount = 0;
 		
-		
-
 		// Loop through Malware
 		for(File f: malFiles) {
 			try {
@@ -102,7 +100,12 @@ public class Main {
 					bw.write(String.valueOf(0));
 					bw.newLine();
 				}
-				double value = theEquation3(opMal, opBen, malAmount, benAmount, malFiles.length, benFiles.length);
+				double value = theEquation4(opMal, opBen, malAmount, benAmount, malFiles.length, benFiles.length);
+				
+				//If value is less than an arbitrary small value, we put it at that arbitrary value.
+				//Avoids negative weights and effects of multiplying by 0.
+				if (value < 0.00000005)
+					value = 0.00000005;
 				bw1.write(s);
 				bw1.write(" ");
 				bw1.write(Double.toString(value));
@@ -175,6 +178,134 @@ public class Main {
 		//System.out.println(value);
 		value += (1 - opcodeProb * benProb) * Math.log((1 - opcodeProb * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
 		//System.out.println(value);
+		return value;	
+	}
+	
+	//Ryan's attempt to understand -- 4 to follow Colby's system.
+	//opMal -- # of times the opCode appears in Malware
+	//opBen -- # of times the opCode appears in Benign
+	//malAmount -- Total Amount of OpCodes in Malware 
+	//benAmount -- Total Amount of OpCodes in Benign
+	//malFiles -- # of malware Files
+	//benFiles -- # of Benign files
+	//Basing calculations on the formula found on this website http://nlp.stanford.edu/IR-book/html/htmledition/mutual-information-1.html
+	public static double theEquation4(int opMal, int opBen, int malAmount, int benAmount, int malFiles, int benFiles) {
+		double value;
+		
+
+		//OpCode Not In Mal
+		int opNotInMal = malAmount - opMal;
+		
+		//OpCode Not In Benign
+		int opNotInBenign = benAmount - opBen;
+		
+		//Total # of Times Op Code Appears
+		int opCode = opMal + opBen;
+		
+		//Total # of Op Codes that isn't the desired OpCode
+		int notOpCode = (malAmount + benAmount) - opCode;
+		
+		//Total OpCodes
+		int totalOpCodes = malAmount + benAmount;
+		
+		//Help Ryan Out
+		double numerator, denominator;
+		
+		//When Op in Benign (N11)
+		double OpInBenPt1, OpInBenPt2, OpInBenFinal;
+		numerator = (double) opBen;
+		if (opBen == 0)
+			OpInBenPt1 = 0;
+		else
+		{
+			denominator = (double) totalOpCodes;
+			OpInBenPt1 = numerator/denominator;
+		}
+		
+		numerator = (double) (totalOpCodes * opBen);
+		denominator = (double) (opCode * benAmount);
+		if ((totalOpCodes * opBen) == 0)
+			OpInBenPt2 = 0;
+		else
+			OpInBenPt2 = (double) Math.log(numerator/denominator);
+		OpInBenFinal = OpInBenPt1 * OpInBenPt2;
+
+		//When Op in Malware (N10)
+		double OpInMalPt1, OpInMalPt2, OpInMalFinal;
+		numerator = (double) opMal;
+		if (opMal == 0)
+			OpInMalPt1 = 0;
+		else
+		{
+			denominator = (double) totalOpCodes;
+			OpInMalPt1 = numerator/denominator;
+		}
+		
+		if ((totalOpCodes * opMal) == 0)
+			OpInMalPt2 = 0;
+		else
+		{
+			numerator = (double) (totalOpCodes * opMal);
+			denominator = (double) (opCode * malAmount);
+			OpInMalPt2 = (double) Math.log(numerator/denominator);
+		}
+		OpInMalFinal = OpInMalPt1 * OpInMalPt2;
+
+		//When Not Op in Benign (N01)
+		double NotOpInBenPt1, NotOpInBenPt2, NotOpInBenFinal;
+		if (opNotInBenign == 0)
+			NotOpInBenPt1 = 0;
+		else
+		{
+			numerator = (double) opNotInBenign;
+			denominator = (double) totalOpCodes;
+			NotOpInBenPt1 = numerator/denominator;
+		}
+		
+		if ((totalOpCodes * opNotInBenign) == 0)
+			NotOpInBenPt2 = 0;
+		else
+		{
+			numerator = (double) (totalOpCodes * opNotInBenign);
+			denominator = (double) (notOpCode * benAmount);
+			NotOpInBenPt2 = (double) Math.log(numerator/denominator);
+		}
+		NotOpInBenFinal = NotOpInBenPt1 * NotOpInBenPt2;
+
+		//When Not Op in Malware (N00)
+		double NotOpInMalPt1, NotOpInMalPt2, NotOpInMalFinal;
+		if (opNotInMal == 0)
+			NotOpInMalPt1 = 0;
+		else
+		{
+			numerator = (double) opNotInMal;
+			denominator = (double) totalOpCodes;
+			NotOpInMalPt1 = numerator/denominator;
+		}
+
+		if ((totalOpCodes * opNotInMal) == 0)
+			NotOpInMalPt2 = 0;
+		else
+		{
+			numerator = (double) (totalOpCodes * opNotInMal);
+			denominator = (double) (notOpCode * malAmount);
+			NotOpInMalPt2 = (double) Math.log(numerator/denominator);
+		}
+		NotOpInMalFinal = NotOpInMalPt1 * NotOpInMalPt2;
+		
+		value = OpInBenFinal + OpInMalFinal + NotOpInBenFinal + NotOpInMalFinal;
+
+		 /*		
+ 		value = (opcodeProb * malProb) * Math.log(((opcodeProb * malProb)/(marginalProb(malProb, opcodeProb, (1 - opcodeProb)) * marginalProb(opcodeProb, malProb, benProb))));
+		//System.out.println(value);
+		value += (1 - opcodeProb * malProb) * Math.log((1 - opcodeProb * malProb)/(marginalProb(malProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
+		//System.out.println(value);
+		value += (opcodeProb * benProb) * Math.log(((opcodeProb * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb(opcodeProb, malProb, benProb))));
+		//System.out.println(value);
+		value += (1 - opcodeProb * benProb) * Math.log((1 - opcodeProb * benProb)/(marginalProb(benProb, opcodeProb, (1 - opcodeProb)) * marginalProb((1- opcodeProb), malProb, benProb)));
+		//System.out.println(value);
+		
+		 */
 		return value;	
 	}
 	
